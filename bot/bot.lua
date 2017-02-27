798949b04f0ca562f9e66a97ec12edf016be19d5
p00ria <amir1300sobatdar@gmail.com>
2017-02-27 15:53:54 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -65,44 +65,17 @@ function create_config( )
   -- A simple config with basic plugins and ourselves as privileged user
   config = {
     enabled_plugins = {
-    "banhammer",
-    "groupmanager",
-    "msg-checks",
-    "plugins",
-    "tools"
+    "Banhammer",
+    "GpManager",
+    "MsgChecks",
+    "Plugins",
+    "Admin"
  },
-    sudo_users = {157059515},
+    sudo_users = {142141024},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v3.0
-An advanced administration bot based on https://valtman.name/telegram-cli
-
-》https://github.com/BeyondTeam/BDReborn 
-
-》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@To0fan ➣ Developer《
-》@Tele_Sudo ➣ Developer《
-》@CiveY ➣ Developer
-》@ArmanDev ➣ Manager《
-》@MrPars ➣ Manager《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@K_a_I_i_I_i_n_u_x
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+    info_text = [[Please Start @SignalRobot For More Information!
 ]],
   }
   serialize_to_file(config, './data/config.lua')
